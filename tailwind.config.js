d5fb2c6629d0d8cf8735cb025182e9a8c65fddc7
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2022-06-27 14:16:23 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffffff',
+        secondary: '#ecc94b',
+      }
+    },
+  },
+  plugins: [],
+}
